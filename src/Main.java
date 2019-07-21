9f7121a20940c8c76807722e3fbfee752ec572b9
BUIQUOCDAT1281997 <31703769+BUIQUOCDAT1281997@users.noreply.github.com>
2019-07-21 06:03:08 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            TuringMachine turingMachine = new TuringMachine("command.txt",
-                    "input.txt", "alphabet.txt");
-            turingMachine.run();
+
+            // 1. Циклический сдвиг влево на заданное число разрядов (оба аргумента в двоичном коде)
+
+            /* TuringMachine turingMachine = new TuringMachine("command.txt",
+           //         "input.txt", "alphabet.txt");
+           turingMachine.run();*/
+
+
+
+            // 2. даны два числа (А и В) в двоичной системе счисления, разделенные знаком "-". Вычислить разность (головка находиться в последной клетки- значить 7)
+            TuringMachine tM = new TuringMachine("command2.txt",
+                    "input2.txt", "alphabet2.txt");
+            tM.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
